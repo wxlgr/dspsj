f066fb555b10da1888b3a171a5979f4b683964ba
wxlgr <1804643502@qq.com>
2023-04-05 18:50:32 +0800

    修改一些文件

--- api-server/index.js
+++ api-server/index.js
@@ -1,18 +1,14 @@
 const express = require('express')
-const  cors = require('cors')
-const  app = express()
- 
+const cors = require('cors')
+const app = express()
 
 const path = require('path')
-
-
-const {APP_PORT} = require('./src/config/app.config')
 
 // 解析body
 app.use(express.json())
 
 // 解析表单中的 url-encoded 格式的数据
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 // 解决跨域
 app.use(cors())
@@ -21,15 +17,17 @@ const router = require('./src/router')
 //挂载路由
 app.use('/api/v1', router)
 // 静态资源文件夹
-app.use("/static",express.static('./static'))
+app.use("/static", express.static('./static'))
 
 
 // 其他
-app.use('*',(req, res) =>{
-  res.status(404).json({code:1,result:null,msg:"not found"})
+app.use('*', (req, res) => {
+  res.status(404).json({ code: 1, result: null, msg: "not found" })
 })
 
 
-app.listen(APP_PORT, () => {
-  console.log(`api-server listening at http://localhost:${APP_PORT}`)
+
+// 监听3000端口
+app.listen(3000, () => {
+  console.log(`api-server listening at http://localhost:3000`)
 })
--- api-server/src/config/app.config.js
+++
@@ -1,4 +0,0 @@
-// 读取.env文件，并添加字段至process.env中
-require('dotenv').config()
-
-module.exports = process.env
